45e00c5e9664f0fa5016c53a9f0e8c50caf0767e
jmichalo <jmichalo@redhat.com>
2022-05-06 18:46:04 +0200

    AddOrUpdateHabyteActivity validation, day of week picker added

--- app/src/main/java/fi/muni/android/habyte/ui/detail/HabyteDetailFragment.kt
+++ app/src/main/java/fi/muni/android/habyte/ui/detail/HabyteDetailFragment.kt
@@ -6,13 +6,11 @@ import android.app.AlertDialog
 import android.content.DialogInterface
 import android.content.Intent
 import android.content.pm.PackageManager
-import android.icu.text.SimpleDateFormat
 import android.icu.util.Calendar
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
